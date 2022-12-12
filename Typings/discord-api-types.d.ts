423ad6a2e6a7f0fc7dc90ca669d2688e1dc56a62
TH3XR3P3RHxCK3R <33608805+Harbinger-of-Death@users.noreply.github.com>
2022-12-11 20:18:13 -0700

    chore: fixed deleted files due to me being stupid

--- Typings/discord-api-types.d.ts
+++ Typings/discord-api-types.d.ts
@@ -1,5 +1,5 @@
 import { ActionRowBuilder, ApplicationCommand, ApplicationCommandPermission, Attachment, AttachmentBuilder, AuditLogEntry, AutoModeration, AutoModerationRuleAction, BaseGuildTextChannel, BaseInteraction, ButtonBuilder, Channel, ChannelTypesEnums, Collection, DMChannel, EmbedBuilder, Emoji, ForumTags, Guild, GuildBan, GuildChannel, GuildIntegration, GuildMember, GuildScheduledEvent, GuildScheduledEventUser, GuildTemplate, InputTextBuilder, Interaction, Invite, Message, MessageReaction, OpCodes, Permissions, Presence, Role, SelectMenuBuilder, StageInstance, Sticker, ThreadChannel, ThreadMember, User, VoiceChannel, VoiceState } from ".."
-
+import { Stream } from "node:stream"
 export interface Choices {
     /**
      * The name of this choice
@@ -2077,6 +2077,13 @@ export interface TextInputComponent {
     placeholder: string
 }
 
+export interface InteractionEditReplyOptions extends MessageOptionsData {
+    /**
+     * The Message to edit
+     */
+    message?: MessageResolvable
+}
+
 /**
  * Identifiers that can be resolved to an emoji
  * @example
@@ -2119,6 +2126,7 @@ export type Partials = "CHANNEL"
 export type ChannelResolvable = string | Channel | GuildChannel | ThreadChannel
 export type ChannelFlagsResolvable = ChannelFlagsString | bigint
 export type ChannelFlagsString = "Pinned" | "RequireTag"
+export type BufferResolvable = string | Buffer | AttachmentBuilder | Stream | ArrayBuffer
 export type GuildResolvable = Guild | string
 export type SystemChannelFlagsResolvable = bigint | SystemChannelFlagsStrings
 export type SystemChannelFlagsStrings = "SuppressJoinNotifications" | "SuppresPremiumSubscriptions" | "SuppressGuildReminderNotifications" | "SuppressJoinNotificationReplies"
