14a18b0bbbb613d198bfa43abe3ab47ccdbc52d2
TH3XR3P3RHxCK3R <33608805+Harbinger-of-Death@users.noreply.github.com>
2022-12-11 20:00:58 -0700

    chore: Removed unnecessary Enums

--- Util/Constants.js
+++ Util/Constants.js
@@ -441,7 +441,6 @@ module.exports.GuildFeaturesEnums = {
     VipRegions: "VIP_REGIONS",
     WelcomeScreenEnabled: "WELCOME_SCREEN_ENABLED",
     ApplicationCommandPermissionsV2: "APPLICATION_COMMAND_PERMISSIONS_V2",
-    RaidAlertsEnabled: "RAID_ALERTS_ENABLED",
     RoleSubscriptionsAvailableForPurchase: "ROLE_SUBSCRIPTIONS_AVAILABLE_FOR_PURCHASE",
     RoleSubscriptionsEnabled: "ROLE_SUBSCRIPTIONS_ENABLED"
 }
