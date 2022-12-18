47cd07315c1d0550f74f47a403b1995b325b5298
TH3XR3P3RHxCK3R <33608805+Harbinger-of-Death@users.noreply.github.com>
2022-12-17 18:25:13 -0700

    fix: Fixed ratelimit handler
    - This makes it handle shared and user scope, not global yet. Im figuring out how to handle those

--- REST/rest.js
+++ REST/rest.js
@@ -66,39 +66,42 @@ class REST {
             if([...urlSearchParams.values()].length) url = url.concat(`?${decodeURIComponent(urlSearchParams)}`)
         }
 
-        if(collection.has(this.ratelimitBucket)) {
-            const rateLimit = collection.get(this.ratelimitBucket)
-            if(rateLimit.route === oldURL) {
-                if(rateLimit.ratelimited) return this.handleRatelimit(oldURL, options)
-            }
+        if(collection.has(`${this.ratelimitBucket}:${oldURL}`)) {
+            const ratelimitData = collection.get(`${this.ratelimitBucket}:${oldURL}`)
+            if(ratelimitData.ratelimited) return this.handleRatelimit(oldURL, options)
         }
         const request = await fetch(url, { method: options.method, agent, signal: controller.signal, headers, body }).finally(() => clearTimeout(timeout))
         this.ratelimitBucket = request.headers.get("X-RateLimit-Bucket")
-        const remaining = parseInt(request.headers.get("X-RateLimit-Remaining"))
+        const limit = +request.headers.get("X-RateLimit-Limit")
+        const remaining = +request.headers.get("X-RateLimit-Remaining")
         const reset = Math.floor((request.headers.get("X-RateLimit-Reset") * 1000) - Date.now())
-        const limit = parseInt(request.headers.get("X-RateLimit-Limit"))
-        if(this.ratelimitBucket) {
-            if(collection.has(this.ratelimitBucket)) {
-                const ratelimit = collection.get(this.ratelimitBucket)
-                if(ratelimit.ratelimited) return;
-                ratelimit.remaining = remaining
-                if(ratelimit.remaining <= 1) ratelimit.ratelimited = true 
-            } else {
-                collection.set(this.ratelimitBucket, { remaining, reset, limit, route: url, ratelimited: remaining <= 1, method: options.method, bucket: this.ratelimitBucket })
+        const response = request.headers.get("content-type")?.includes("application/json") ? await request.json() : null
+        const scope = request.headers.get("x-ratelimit-scope")
+        if(!collection.has(`${this.ratelimitBucket}:${oldURL}`)) {
+            if(remaining <= 1) collection.set(`${this.ratelimitBucket}:${oldURL}`, { limit, remaining, reset, bucket: this.ratelimitBucket, method: options.method, route: oldURL, ratelimited: true })
+        }
+        
+        if(request.status === 429) {
+            switch(scope) {
+                case "shared":
+                    collection.set(`${this.ratelimitBucket}:${oldURL}`, { retryAfter: Math.floor(response.retry_after * 1000), global: response.global, ratelimited: true })
+                    return this.handleRatelimit(oldURL, options)
+                case "user":
+                    return this.handleRatelimit(oldURL, options)
             }
         }
-        const response = request.headers.get("content-type")?.includes("application/json") ? await request.json() : undefined
-        if(request.status === 429) return await this.handleRatelimit(oldURL, options)
         if(!request.ok) throw new DiscordAPIError({
-            message: response.message ?? response.error_description ?? request.statusText,
+            message: response?.message ?? response.error_description ?? request.statusText,
             method: options.method,
-            code: response.code,
+            code: response?.code,
             httpError: request.status,
             path: url,
-            rawError: response.errors ?? {},
+            rawError: response?.errors ?? {},
             requestBody: options.auth ?? options.body,
         })
-        if(collection.size > 0) this.clearCollection(reset)
+
+        if(collection.size) this.clearCollection(reset)
+
         return response
     }
 
@@ -138,10 +141,10 @@ class REST {
     }
     
     async handleRatelimit(oldUrl, options) {
-        const ratelimitData = collection.get(this.ratelimitBucket)
+        const ratelimitData = collection.get(`${this.ratelimitBucket}:${oldUrl}`)
         if(ratelimitData) {
             this.client.emit(EventTypes.Ratelimit, ratelimitData)
-            await wait(ratelimitData.reset)
+            await wait(ratelimitData.reset ?? ratelimitData.retryAfter)
             ratelimitData.ratelimited = false
             return await this._make(oldUrl, options)
         }
